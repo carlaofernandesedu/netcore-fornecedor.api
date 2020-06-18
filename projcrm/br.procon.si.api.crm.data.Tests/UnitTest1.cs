c2226a40a1a38ff7a7449926aef91d7a5bc961fa
carlos melo <carlaofernandesedu@gmail.com>
2020-06-18 01:28:00 -0300

    Projeto de Testes e Estrutura Basica

---
+++ projcrm/br.procon.si.api.crm.data.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace br.procon.si.api.crm.data.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
