329e66afead8dcf4374b4d6799017fb5289db0ff
carlos melo <carlaofernandesedu@gmail.com>
2020-06-19 01:00:59 -0300

    Implementacao Tests

---
+++ projcrm/br.procon.si.api.crm.data.Tests/CRMHelperServerComunicationTest.cs
@@ -0,0 +1,166 @@
+﻿﻿using br.procon.si.api.crm.data.Standard.RestHttp;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace br.procon.si.api.crm.data.Tests
+{
+   
+    public class CRMHelperServerComunicationTest
+    {
+        private CRMHelperServerComunication _cliente;
+        private string _url;
+        private HttpMethod _metodo;
+        private string _body;
+
+        [Fact]
+        [Trait("Category","ComunicacaoSimples")]
+        public void QuandoSolicitoPorGETSimplesEntaoOK()
+        {
+            ///ARRANGE
+            _url = "http://www.g1.com.br";
+            _metodo = HttpMethod.Get;
+            _cliente = CRMHelperServerComunication.Construir();
+            ///
+            ///ACT
+            var response = _cliente.Enviar(_url, _metodo).Result;
+            ///ASSERT
+            var mensagemRetorno = ((int)response.CodigoRetorno) == 0 ? response.MensagemException : ((int)response.CodigoRetorno).ToString();
+            Assert.True(response.ExecutadoComSucesso,mensagemRetorno);
+        }
+
+        [Fact]
+        [Trait("Category","ComunicacaoObjeto")]
+        public void QuandoSolicitorPorGETObjetoEntaoRetornoObjetoOK()
+        {
+            ///ARRANGE
+            _url = "http://desenv.mobile.sp.gov.br:90/servicosweb/api/atendimento/dominios/meiosaquisicoes";
+            _metodo = HttpMethod.Get;
+            _cliente = CRMHelperServerComunication.Construir();
+            ///
+            ///ACT
+            var response = _cliente.Enviar<List<DominioTestDados>>(_url, _metodo).Result;
+            ///ASSERT
+            var mensagemRetorno = ((int)response.CodigoRetorno) == 0 ? response.MensagemException : ((int)response.CodigoRetorno).ToString();
+            Assert.True(response.ExecutadoComSucesso, mensagemRetorno);
+        }
+
+        [Fact]
+        [Trait("Category","ComunicacaoObjeto")]
+        public void QuandoSolicitorPorGETObjeto_E_RedefinirCabecalhoEntaoRetornoObjetoOK()
+        {
+            ///ARRANGE
+            _url = "http://desenv.mobile.sp.gov.br:90/servicosweb/api/atendimento/dominios/meiosaquisicoes";
+            _metodo = HttpMethod.Get;
+            _cliente = CRMHelperServerComunication.Construir().RedefinirCabecalhos();
+            ///
+            ///ACT
+            var response = _cliente.Enviar<List<DominioTestDados>>(_url, _metodo).Result;
+            ///ASSERT
+            var mensagemRetorno = ((int)response.CodigoRetorno) == 0 ? response.MensagemException : ((int)response.CodigoRetorno).ToString();
+            Assert.True(response.ExecutadoComSucesso,mensagemRetorno);
+        }
+
+        [Fact]
+        [Trait("Category","ComunicacaoSimples")]
+        public void QuandoSolicitoPorPostSimplesRetornarOK()
+        {
+            ///ARRANGE
+            _url = "http://desenv.mobile.sp.gov.br:90/servicosweb/api/seguranca/token";
+            _metodo = HttpMethod.Post;
+            _cliente = CRMHelperServerComunication.Construir();
+            var usuario = "carlos21@gmail.com";
+            var senha = "123456";
+            var encondingUsuarioHtml = usuario.Replace("@", "%40");
+            _body = String.Concat("username=", encondingUsuarioHtml, "&password=", senha, "&grant_type=password");
+            ///
+            ///ACT
+            var response = _cliente.Enviar(_url, _metodo, null, _body).Result;
+            ///ASSERT
+            var mensagemRetorno = ((int)response.CodigoRetorno) == 0 ? response.MensagemException : ((int)response.CodigoRetorno).ToString();
+            Assert.True(response.ExecutadoComSucesso, mensagemRetorno);
+        }
+
+        [Fact]
+        [Trait("Category","ComunicacaoSimples")]
+        public void QuandoSolicitoPorPostObjetoRetornarTokenAutenticacao()
+        {
+            ///ARRANGE
+            _url = "http://desenv.mobile.sp.gov.br:90/servicosweb/api/seguranca/token";
+            _metodo = HttpMethod.Post;
+            _cliente = CRMHelperServerComunication.Construir();
+            var usuario = "carlos21@gmail.com";
+            var senha = "123456";
+            var encondingUsuarioHtml = usuario.Replace("@", "%40");
+            _body = String.Concat("username=", encondingUsuarioHtml, "&password=", senha, "&grant_type=password");
+            ///
+            ///ACT
+            var response = _cliente.Enviar<AutenticacaoTestDados>(_url, _metodo, null, _body).Result;
+            ///ASSERT
+            var mensagemRetorno = ((int)response.CodigoRetorno) == 0 ? response.MensagemException : ((int)response.CodigoRetorno).ToString();
+            Assert.True(response.ExecutadoComSucesso,mensagemRetorno);
+        }
+
+        [Fact]
+        [Trait("Category","ComunicacaoSimples")]
+        public void QuandoSolicitorPorPUTSimplesEntaoOK()
+        {
+            ///ARRANGE
+            _url = "http://desenv.mobile.sp.gov.br:90/servicosweb/api/atendimento/dominios/fornecedores";
+            _metodo = HttpMethod.Put;
+            _cliente = CRMHelperServerComunication.Construir();
+            var objeto = new FornecedorTestDados() { Nome = "vivo", EhCNPJ = true, NumDocumento = null };
+            _body = JsonConvert.SerializeObject(objeto);
+
+            ///
+            ///ACT
+            var response = _cliente.Enviar(_url, _metodo,null,_body,"application/json").Result;
+            ///ASSERT
+            var mensagemRetorno = ((int)response.CodigoRetorno) == 0 ? response.MensagemException : ((int)response.CodigoRetorno).ToString();
+            Assert.True(response.ExecutadoComSucesso, mensagemRetorno);
+        }
+
+        [Fact]
+        [Trait("Category","ComunicacaoSimples")]
+        public void QuandoSolicitorPorPUTObjetoEntaoRetornoObjetoOK()
+        {
+            ///ARRANGE
+            _url = "http://desenv.mobile.sp.gov.br:90/servicosweb/api/atendimento/dominios/fornecedores";
+            _metodo = HttpMethod.Put;
+            _cliente = CRMHelperServerComunication.Construir();
+            var objeto = new FornecedorTestDados() { Nome = "vivo", EhCNPJ = true, NumDocumento = null };
+            _body = JsonConvert.SerializeObject(objeto);
+
+            ///
+            ///ACT
+            var response = _cliente.Enviar<List<FornecedorTestDados>>(_url, _metodo, null, _body, "application/json").Result;
+            ///ASSERT
+            var mensagemRetorno = ((int)response.CodigoRetorno) == 0 ? response.MensagemException : ((int)response.CodigoRetorno).ToString();
+            Assert.True(response.ExecutadoComSucesso, mensagemRetorno);
+        }
+
+        [Fact]
+        [Trait("Category","ComunicacaoSimples")]
+        public void QuandoSolicitorPorPUTObjetoRegistroNaoExisteEntao404()
+        {
+            ///ARRANGE
+            _url = "http://desenv.mobile.sp.gov.br:90/servicosweb/api/atendimento/dominios/fornecedores";
+            _metodo = HttpMethod.Put;
+            _cliente = CRMHelperServerComunication.Construir();
+            var objeto = new FornecedorTestDados() { Nome = "zzzz", EhCNPJ = true, NumDocumento = null };
+            _body = JsonConvert.SerializeObject(objeto);
+
+            ///
+            ///ACT
+            var response = _cliente.Enviar(_url, _metodo, null, _body, "application/json").Result;
+            ///ASSERT
+            var mensagemRetorno = ((int)response.CodigoRetorno) == 0 ? response.MensagemException : ((int)response.CodigoRetorno).ToString();
+            Assert.True(404 == (int)response.CodigoRetorno,mensagemRetorno);
+        }
+    }
+}
---
+++ projcrm/br.procon.si.api.crm.data.Tests/CRMHelperTestDados.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace br.procon.si.api.crm.data.Tests
+{
+    public class CRMHelperTestDados
+    {
+    }
+    [Serializable]
+    public class DominioTestDados
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+
+    public class AutenticacaoTestDados
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+    }
+
+    public class FornecedorTestDados
+    {
+        public string Nome { get; set; }
+        public string NumDocumento { get; set; }
+        public bool EhCNPJ { get; set; }
+        public int? IdFornecedor { get; set; }
+    }
+
+    public class FichaTestDados
+    {
+        public int Codigo { get; set; }
+        public string Protocolo { get; set; } 
+    }
+}
