9ec14ee47bab341f819bc3439605bcc782407e2a
carlos melo <carlaofernandesedu@gmail.com>
2020-06-21 09:19:24 -0300

    Implementado Injecao Depedencias

--- projcrm/br.procon.si.api.crm/Controllers/ValuesController.cs
+++ projcrm/br.procon.si.api.crm/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using br.procon.si.api.crm.domain.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace br.procon.si.api.crm.Controllers
@@ -10,6 +11,11 @@ namespace br.procon.si.api.crm.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly IEventoService _eventoService;
+        public ValuesController(IEventoService eventoService)        
+        {
+            _eventoService = eventoService;
+        }
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
@@ -24,22 +30,6 @@ namespace br.procon.si.api.crm.Controllers
             return "value";
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+       
     }
 }
