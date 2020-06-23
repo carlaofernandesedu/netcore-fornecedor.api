230424c383c508b2f59b33121fe5ccbe1c53b2aa
carlos melo <carlaofernandesedu@gmail.com>
2020-06-23 17:09:36 -0300

    Tratamento Option Pattern

--- projcrm/br.procon.si.api.crm.domain/Services/EventoScopedService.cs
+++ projcrm/br.procon.si.api.crm.domain/Services/EventoScopedService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
+using br.procon.si.api.crm.data.Configuration;
 using br.procon.si.api.crm.data.Interfaces;
 using br.procon.si.api.crm.domain.Interfaces;
 using br.procon.si.api.crm.infra.HostApp;
@@ -15,15 +16,23 @@ namespace br.procon.si.api.crm.domain.Services
         private readonly IProconRepository _repositorioProcon;
         private readonly ICrmRepository _repositorioCrm;
         private readonly IMapper _servicoMapper;
+
+        private readonly DBSettings _dbSettingsOptions;
+
+        private readonly CrmApiSettings _crmApiSettingsOptions;
         private int executionCount = 0;
         private readonly ILogger _logger;
         public EventoScopedService(IProconRepository repositorioProcon, ICrmRepository repositorioCrm, IMapper servicoMapper,
+        IOptions<DBSettings> dbsettingsOptions,
+        IOptions<CrmApiSettings> crmApisettingsOptions,
         ILogger<EventoScopedService> logger)
         {
             _repositorioProcon = repositorioProcon;
             _repositorioCrm = repositorioCrm;
             _servicoMapper = servicoMapper;
              _logger = logger;
+             _crmApiSettingsOptions = crmApisettingsOptions.Value;
+             _dbSettingsOptions = dbsettingsOptions.Value;
         }
 
         public async Task DoWork(CancellationToken stoppingToken)
